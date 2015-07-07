417df90bb13ac1c2179efa7d08e8c6e5052a2415
Valerie Montoya <valeriemontoya90@gmail.com>
2015-07-08 00:06:03 +0200

    test about the KnpLabs/php-github-api work, we can get all infos we need now

--- src/FindWorkerBundle/Controller/DefaultController.php
+++ src/FindWorkerBundle/Controller/DefaultController.php
@@ -2,12 +2,42 @@
 
 namespace FindWorkerBundle\Controller;
 
+require_once '../vendor/autoload.php';
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Github\Api\ApiInterface;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('FindWorkerBundle:Default:index.html.twig', array('name' => $name));
+		$client = new \Github\Client();
+		
+		$client->setOption('api_limit', 60);
+
+		//ne récupère que les 30 premiers résultats; comment récupérer les 311540 (en entier) ?
+		$profils = $client->api('search')->users('language:php');
+
+		foreach($profils["items"] as $key => $value) {
+			//get infos' user
+			$username = $value["login"];
+			$user = $client->api('user')->show($username);
+			var_dump($user);
+		}
+
+        return $this->render('FindWorkerBundle:Default:index.html.twig');
     }
+
+    /* method ne marche pas : Attempted to call function "findInfoByUser" from namespace "FindWorkerBundle\Controller".
+    public function findInfoByUser($username) {
+		//get infos' user
+		$user = $client->api('user')->show($username);
+		var_dump($user);
+		
+		//print $user['login'];
+		//print $user['email'];
+		//print $user['following'];
+		//print $user['public_repos'];
+	
+    }*/
 }
